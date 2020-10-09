a5c9bab25a26984423304ea0832e73d6492870f8
Valentyn <valentynd91@gmail.com>
2020-10-09 17:40:06 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <TotalBalance :total="totalBalance" />
+    <BudgetList :list="list" @deleteItem="onDeleteItem" />
+  </div>
+</template>
+
+<script>
+import BudgetList from '@/components/BudgetList';
+import TotalBalance from '@/components/TotalBalance';
+
+export default {
+  name: 'App',
+
+  components: {
+    BudgetList,
+    TotalBalance,
+  },
+
+  data: () => ({
+    list: {
+      1: {
+        type: 'INCOME',
+        value: 100,
+        comment: 'Some comment',
+        id: 1,
+      },
+      2: {
+        type: 'OUTCOME',
+        value: -50,
+        comment: 'Some comment',
+        id: 2,
+      },
+    },
+  }),
+
+  computed: {
+    totalBalance() {
+      return Object.values(this.list).reduce(
+        (acc, item) => acc + item.value,
+        0
+      );
+    },
+  },
+
+  methods: {
+    onDeleteItem(id) {
+      this.$delete(this.list, id);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,26 @@
+<template>
+  <ElCard>
+    <ElForm :model="formData">
+      <ElFormItem label="Type" prop="type">
+        <ElSelect>
+          <ElOption></ElOption>
+        </ElSelect>
+      </ElFormItem>
+    </ElForm>
+  </ElCard>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  data: () => ({
+    formData: {
+      type: 'INCOME',
+      comment: '',
+      value: 0,
+    },
+  }),
+};
+</script>
+
+<style scoped></style>
